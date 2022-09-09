63a39ae37bc717442c11d9cab3e893cfee7b1cfa
J05hL <josh.langford@hotmail.co.uk>
2022-09-09 12:12:59 +0100

    repackage data within fetch function now repackages data into the structure expected by our front end

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -6,7 +6,7 @@ console.log(process.env.API_KEY)
 
 const baseUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=`
 
-const query = `&cuisine=italian`
+const query = `&cuisine=italian&fillIngredients=true`
 
 export const getMenu = async () => {
 
@@ -16,6 +16,35 @@ export const getMenu = async () => {
 
   const data = await response.json();
 
-    
-  return data
+     
+  return createNewArray(data.results)
+}
+
+//PLAN TAKE array of objects change to array of objects with only what we need
+// we need from dummyData.results[i].title
+// - name = dummyData.results[i].title
+// - image link = dummyData.results[i].image 
+// - dummyData.results[i].missedIngredients.map((item)=> item.originalName)
+// - add random price
+
+const createNewArray = (array) => {
+
+  return array.map((item)=> restructureObject(item))
+
+}
+
+const restructureObject = (obj) => {
+
+  const {title, image, missedIngredients} = obj
+
+  const ingredients = missedIngredients.map((item)=> item.originalName)
+
+  const newObj = {
+    title, 
+    image,
+    ingredients: ingredients
+  }
+
+  return newObj
+  
 }
