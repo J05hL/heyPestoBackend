905475114bc67e9942fd38ff37411896ec4978b0
J05hL <josh.langford@hotmail.co.uk>
2022-09-09 12:39:06 +0100

    added try catch to the menu get route

--- menu/menu.js
+++ menu/menu.js
@@ -6,14 +6,16 @@ dotenv.config()
 
 import {getMenu} from '../fetch/fetch.js '
 
-/* GET users listing. */
 router.get("/", async function (req, res, next) {
 
   const data = await getMenu()
-
-  console.log(`this is data`, data)
+ 
+  try {
+    res.status(201).json({ success: true, payload: data });
+  } catch (err) {
+    res.status(400).json({success: false, message: err.message });
+  }
   
-  res.json({ message: data });
 });
 
 export default router;
