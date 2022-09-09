5912ae6ed3ff0ffadca7499ab28ada78e7c38920
J05hL <josh.langford@hotmail.co.uk>
2022-09-09 12:14:14 +0100

    removed console logs from fetch.js

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -2,21 +2,16 @@ import fetch from 'node-fetch'
 import * as dotenv from 'dotenv' 
 dotenv.config({path: '../.env'})
 
-console.log(process.env.API_KEY)
-
 const baseUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=`
 
 const query = `&cuisine=italian&fillIngredients=true`
 
 export const getMenu = async () => {
 
-  console.log(`this is our url`, `${baseUrl}${process.env.API_KEY}${query}`)
-
   const response = await fetch(`${baseUrl}${process.env.API_KEY}${query}`);
 
   const data = await response.json();
 
-     
   return createNewArray(data.results)
 }
 
